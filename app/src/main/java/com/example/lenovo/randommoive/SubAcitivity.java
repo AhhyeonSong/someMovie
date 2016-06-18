53704cca2ab2fc2dc289300cc578f0dfe7a4aaec
unknown <>
2016-06-18 13:52:13 +0900

    #1 Main UI

---
+++ app/src/main/java/com/example/lenovo/randommoive/SubAcitivity.java
@@ -0,0 +1,15 @@
+package com.example.lenovo.randommoive;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Lenovo on 2016-06-18.
+ */
+public class SubAcitivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub01);  // layout xml 과 자바파일을 연결
+    }
+}
